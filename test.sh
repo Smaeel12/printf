195dfc86f78aa5baa74773828cb3fcf77c93259e
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-02 19:59:41 +0000

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-file_name="example.txt"; window_width=$(stty size | cut -d" " -f2)
-underscores=$(printf "%*s" $(( (window_width - ${#file_name} - 4) / 2 )) "" | tr ' ' '_')
-printf "%s %s %s\n" "$underscores" "$file_name" "$underscores"
-
